9065fb5b60dfaeea0140c076cbe1d5922251490e
snehabirodkar <snehabirodkar31@gmail.com>
2024-06-29 22:29:08 +0530

    chapter 03 updated

--- Chapter 03 - Laying Foundation/App.js
+++ Chapter 03 - Laying Foundation/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import logo from "./assets/download.jpg";
 
 // const heading = React.createElement(
 //   "h1",
@@ -23,6 +25,42 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 // root.render(jsxHeading);
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id:"title",
+//     className:"heading",
+//     style:{color:"red"}
+//   },
+//   "Namaste React!!!"
+// )
+
+// setTimeout(
+//   function(){
+//     const heading3 = document.getElementById('title');
+//     console.log(heading3)
+//     heading3.innerHTML = 'Namaste JS'
+//   }
+//   , 2000)
+
+// root.render(heading)
+
+// --------------------------------------------------------------
+// - class based Components - OLD
+
+// const h1 = React.createElement("h1", { key: "2", id: "key" }, "Heading 1");
+
+// const h2 = React.createElement("h2", { key: "3", id: "key" }, "Heading 2");
+
+// const container = React.createElement("div", { id: "container" }, [
+//   heading,
+//   heading2,
+//   h1,
+//   h2,
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // -------------------------------------------------------------//
 
@@ -50,4 +88,26 @@ const HeadingComponent = () => (
 //{} - jsx sanitize the code written in it.
 //Element will be rendered - roor.render(jsxHeading);
 // Functional component will render - root.render(<HeadingComponent />)
-root.render(<HeadingComponent />);
+
+// ---------------- Assignmnet ------------------------
+const HeaderAssignment = () => (
+  <div className="container">
+    <div className="logo p-10">
+      <img
+        className="logo-img"
+        src={logo}
+        alt=""
+      />
+    </div>
+    <div className="search-bar p-10">
+      <label className="p-10">Search</label>
+      <input
+        className="search-input"
+        type="text"
+      />
+    </div>
+    <div className="user-icon p-10">user icon</div>
+  </div>
+);
+root.render(<HeaderAssignment />);
+// root.render(<HeadingComponent />);
