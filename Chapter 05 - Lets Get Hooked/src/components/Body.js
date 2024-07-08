4dcc8c40edb508d3e128f7358ca905e00c244443
snehabirodkar <snehabirodkar31@gmail.com>
2024-07-08 09:54:16 +0530

    search added

--- Chapter 05 - Lets Get Hooked/src/components/Body.js
+++ Chapter 05 - Lets Get Hooked/src/components/Body.js
@@ -3,14 +3,42 @@ import resLis from "../utils/mockData";
 import { useState } from "react";
 
 export const Body = () => {
-  //Local state variable
-
   const [restaurantList, setListOfRestraunt] = useState(resLis);
+  const [searchText, setSearchRestaurant] = useState("");
+  // Its array destructuring
 
+  // const restaurantList = arr[0];
+  // const setListOfRestraunt = arr[1];
+  function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>
+      restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    return filterData;
+  }
   return (
     <div className="body">
       <div className="d-flex">
-        <div className="search">Search</div>
+        <div className="search">
+          Search
+          <input
+            type="text"
+            placeholder="search resto"
+            value={searchText}
+            onChange={(e) => {
+              setSearchRestaurant(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              // filter the data
+              const data = filterData(searchText, restaurantList);
+              // update the state of restaurants list
+              setListOfRestraunt(data);
+            }}>
+            Search
+          </button>
+        </div>
         <button
           className="filter"
           onClick={() => {
